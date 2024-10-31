dfa64dacdc3798b6b1115f47bb8ac36462d1b68e
i.shevelev <i.shevelev@timeweb.ru>
2024-10-31 21:02:28 +0300

    Add mysql connection

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db-check', function () {
+    try {
+        \DB::connection()->getPdo();
+        return 'Соединение с базой данных установлено!';
+    } catch (\Exception $e) {
+        return 'Не удалось подключиться к базе данных: ' . $e->getMessage();
+    }
+});
